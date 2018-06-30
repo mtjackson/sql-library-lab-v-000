2413f7ed98683d26bf9eb8da4ce529673cdc0250
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 11:22:53 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, SUM(species) FROM characters "
+  "SELECT species, COUNT(species) FROM characters WHERE "
 end
 
 def select_name_and_series_subgenres_of_authors
