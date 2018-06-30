dc568ae96c6c56032c8967b6e28a15a75f7b9083
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 10:49:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,4 @@
 INSERT INTO series (title) VALUES ('Space Adventures 2000'), ('Lost Continent');
 
-INSERT INTO books (title, year)
+INSERT INTO books (title, year) VALUES ('Martian Attack', 1973),
+  ('Interstellar Gravediggers', 1954), ()
