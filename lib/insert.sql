b7232f9b39fdfb6b018c3eb0eb559931edb1ba3b
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 10:59:19 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -15,4 +15,4 @@ INSERT INTO characters (name, species, motto) VALUES
   ('Dirk Gutpuncher', 'Human', 'Hello, ladies...'),
   ('Adriann', 'Human', '*stares intensely*');
 
-INSERT INTO subgenres VALUES
+INSERT INTO subgenres (name) VALUES 
