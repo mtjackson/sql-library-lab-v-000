77ecc0e07a28f94b31546b9ebeb3d5f8ef298436
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 11:56:29 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,6 +23,6 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.character_id) FROM characters
-    INNER JOIN character_books WHERE characters.id = character_books.character_id
+    INNER JOIN character_books ON characters.id = character_books.character_id
     ORDER BY COUNT(character_books.character_id) DESC;"
 end
