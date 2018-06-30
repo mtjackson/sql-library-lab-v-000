b46430ba6119028f262e39e06a094f9f493cc1ed
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 11:31:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,8 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT author.name, subgenres.name FROM author, subgenres INNER JOIN series"
+  "SELECT author.name, subgenres.name FROM author, subgenres INNER JOIN series
+    WHERE"
 end
 
 def select_series_title_with_most_human_characters
