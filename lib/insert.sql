d03cfb888a5aa72c5a2e1ccfa64cb9724419990a
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 10:54:38 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -9,4 +9,4 @@ INSERT INTO characters (name, species, motto) VALUES
   ('Captain Leo', 'Human', 'Not on my watch, buckaroos'),
   ('Captain Xander', 'Human', 'The sun never sets on the British Empire...in SPACE'),
   ('Admiral Zelda', 'Human (or is she...)', 'Full speed ahead'),
-  
+  ('Lex', 'Alien', '*indistinct growls that are kind of cute*')
