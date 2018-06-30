1d3182149a5b34617417a514869f63c3f15efff9
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 11:53:50 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,5 +23,5 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.character_id) FROM characters, character_books
-    WHERE characters.id = character_books.character_id"
+    WHERE characters.id = character_books.character_id;"
 end
