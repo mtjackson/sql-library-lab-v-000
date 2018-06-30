618d2da67f3b72413556b84f379505930feee78f
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 11:40:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series INNER JOIN characters ON series.id = characters.series_id
-    GROUP BY characters.species = (SELECT MAX(COUNT(species)) FROM characters);"
+    GROUP BY characters.species ORDER BY COUNT(chracters.species)"
 end
 
 def select_character_names_and_number_of_books_they_are_in
