4afb4bb8272c081c94d86a6cd0ba87346356d16d
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 11:06:18 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -21,4 +21,4 @@ INSERT INTO authors (name) VALUES ('Joe "Definitely Not a Pen Name" Vegas'),
   ('Alexi Volkava');
 
 INSERT INTO character_books (book_id, character_id) VALUES
-  (1, 1), (1, 2), (1, 3), (2, 2), (2, 4), (3, 1), (3, 3), (4, 4)
+  (1, 1), (1, 2), (1, 3), (2, 2), (2, 4), (3, 1), (3, 3), (4, 4), (4, 1)
