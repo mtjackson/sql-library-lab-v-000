7d7b7319208936a21862c1963a4b700206e81d81
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 11:55:56 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,6 +22,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.character_id) FROM characters, character_books
+  "SELECT characters.name, COUNT(character_books.character_id) FROM characters INNER JOIN character_books
     WHERE characters.id = character_books.character_id ORDER BY COUNT(character_books.character_id) DESC;"
 end
