70f3eaa47f24c90dc8d417bb9702a75979a7f598
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 10:53:14 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,4 +6,6 @@ INSERT INTO books (title, year) VALUES ('Martian Attack', 1973),
   ('911...in SPACE', 1960);
 
 INSERT INTO characters (name, species, motto) VALUES
-  ('Captain Leo', 'Human', 'Not on my watch, buckaroos'), 
+  ('Captain Leo', 'Human', 'Not on my watch, buckaroos'),
+  ('Captain Xander', 'Human', 'The sun never sets on the British Empire...in SPACE'),
+  
