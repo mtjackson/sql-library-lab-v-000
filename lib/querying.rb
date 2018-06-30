278389b6a930cc54f92719f99a523b843a342395
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 11:31:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT author.name, subgenres.name FROM author, subgenres INNER JOIN series
-    WHERE"
+    ON"
 end
 
 def select_series_title_with_most_human_characters
