9962c285126558e6666a33c26d1c21e148bb5359
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-30 11:32:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT author.name, subgenres.name FROM author, subgenres INNER JOIN series
-    ON author.id = series.author_id"
+    ON author.id = series.author_id;"
 end
 
 def select_series_title_with_most_human_characters
